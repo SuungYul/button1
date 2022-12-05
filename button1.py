083a770364d32d3c82199ccb860c463acc0e9fca
SuungYul <pmtf00@gmail.com>
2022-12-05 10:20:32 +0900

    button change

--- button1.py
+++ button1.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
-window =Tk()
+window = Tk()
 
-b1 = Button(window, text="Yeonseung Ryu")
-b2 = Button(window, text="60161111")
+b1 = Button(window, text="박성열")
+b2 = Button(window, text="60192192")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
